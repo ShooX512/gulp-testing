c7904a9aada548a88275ffa3184d70dfbc3f42b2
ShooX512 <47025005+ShooX512@users.noreply.github.com>
2019-03-27 20:21:33 +0000

    First test

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp'),
+    pug = require('gulp-pug'),
+    data = require('gulp-data'),
+    fs = require('fs');
+
+function defaultTask(cb) {
+  // place code for your default task here
+  cb();
+}
+
+function pugTest () {
+    return gulp
+        .src('app/views/**/*.pug')
+        .pipe(data(function(file) {
+            return JSON.parse(fs.readFileSync('./app/data.json'))
+        }))
+        .pipe(pug())
+        .pipe(gulp.dest('/dist'));
+}
+
+exports.default = gulp.series(defaultTask, pugTest)
